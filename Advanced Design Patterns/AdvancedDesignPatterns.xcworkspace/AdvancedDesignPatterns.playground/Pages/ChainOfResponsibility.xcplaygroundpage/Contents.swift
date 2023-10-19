ebc94a0578489db4a18e8a75ed07131a0fd719ca
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2023-10-19 19:31:57 +0300

    Add chain of responsibility pattern

--- Advanced Design Patterns/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/ChainOfResponsibility.xcplaygroundpage/Contents.swift
+++ Advanced Design Patterns/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/ChainOfResponsibility.xcplaygroundpage/Contents.swift
@@ -71,3 +71,107 @@ public class Quarter: Coin {
   public override class var standardWeight: Double { return 5.670 }
   public override var centValue: Int { return 25 }
 }
+
+// MARK: - HandlerProtocol
+public protocol CoinHandlerProtocol {
+  var next: CoinHandlerProtocol? { get }
+  func handleCoinValidation(_ unknownCoin: Coin) -> Coin?
+}
+
+// MARK: - Concrete Handler
+public class CoinHandler {
+  public var next: CoinHandlerProtocol?
+  public let coinType: Coin.Type
+  public let diameterRange: ClosedRange<Double>
+  public let weightRange: ClosedRange<Double>
+  
+  public init(coinType: Coin.Type,
+              diameterVariation: Double = 0.05,
+              weightVariation: Double = 0.05) {
+    self.coinType = coinType
+    
+    let standardDiameter = coinType.standardDiameter
+    self.diameterRange =
+      (1-diameterVariation)*standardDiameter ...
+      (1+diameterVariation)*standardDiameter
+    
+    let standardWeight = coinType.standardWeight
+    self.weightRange =
+      (1-weightVariation)*standardWeight ...
+      (1+weightVariation)*standardWeight
+  }
+}
+
+extension CoinHandler: CoinHandlerProtocol {
+  public func handleCoinValidation(_ unknownCoin: Coin) -> Coin? {
+    guard let coin = createCoin(unknownCoin) else {
+      return next?.handleCoinValidation(unknownCoin)
+    }
+    return coin
+  }
+  
+  private func createCoin(_ unknownCoin: Coin) -> Coin? {
+      print("Attempt to create \(coinType)")
+    guard diameterRange.contains(unknownCoin.diameter) else {
+      print("Invalid diameter")
+      return nil
+    }
+    guard weightRange.contains(unknownCoin.weight) else {
+      print("Invalid weight")
+      return nil
+    }
+    let coin = coinType.init(diameter: unknownCoin.diameter, weight: unknownCoin.weight)
+    print("Created \(coin)")
+    return coin
+  }
+}
+
+// MARK: - Client
+public class VendingMachine {
+  public let coinHandler: CoinHandler
+  public var coins: [Coin] = []
+  
+  public init(coinHandler: CoinHandler) {
+    self.coinHandler = coinHandler
+  }
+  
+  public func insertCoin(_ unknownCoin: Coin) {
+    guard let coin = coinHandler.handleCoinValidation(unknownCoin) else {
+      print("Coin rejected: \(unknownCoin)")
+      return
+    }
+    print("Coin accepted: \(coin)")
+    coins.append(coin)
+    
+    let dollarValue = coins.reduce(0) { $0 + $1.dollarValue }
+    print("")
+    print("Coins total value: $\(dollarValue)")
+    
+    let weight = coins.reduce(0) { $0 + $1.weight }
+    print("Coins total weight: \(weight) g")
+    print("")
+  }
+}
+
+// MARK: - Example
+let pennyHandler = CoinHandler(coinType: Penny.self)
+let nickelHandler = CoinHandler(coinType: Nickel.self)
+let dimeHandler = CoinHandler(coinType: Dime.self)
+let quarterHandler = CoinHandler(coinType: Quarter.self)
+
+pennyHandler.next = nickelHandler
+nickelHandler.next = dimeHandler
+dimeHandler.next = quarterHandler
+
+let vendingMachine = VendingMachine(coinHandler: pennyHandler)
+
+let penny = Penny()
+vendingMachine.insertCoin(penny)
+
+let quarter = Coin(diameter: Quarter.standardDiameter,
+                   weight: Quarter.standardWeight)
+vendingMachine.insertCoin(quarter)
+
+let invalidDime = Coin(diameter: Quarter.standardDiameter,
+                       weight: Dime.standardWeight)
+vendingMachine.insertCoin(invalidDime)
