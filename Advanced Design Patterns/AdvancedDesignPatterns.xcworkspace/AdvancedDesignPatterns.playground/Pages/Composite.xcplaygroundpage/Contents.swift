78486637dbdd03626cf62eade55cf6207fb78b2f
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2023-10-19 16:34:56 +0300

    Add Composite pattern

--- Advanced Design Patterns/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Composite.xcplaygroundpage/Contents.swift
+++ Advanced Design Patterns/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Composite.xcplaygroundpage/Contents.swift
@@ -13,3 +13,67 @@
  
  ## Code Example
  */
+import Foundation
+
+protocol File {
+  var name: String { get set }
+  func open()
+}
+
+struct eBook: File {
+  var name: String
+  var author: String
+  
+  func open() {
+    print("Opening \(name) by \(author) in iBooks...")
+  }
+}
+
+struct Music: File {
+  var name: String
+  var artist: String
+  
+  func open() {
+    print("Opening \(name) by \(artist) in Music...")
+  }
+}
+
+struct Folder: File {
+  var name: String
+  var files: [File] = []
+  
+  mutating func addFile(_ file: File) {
+    self.files.append(file)
+  }
+  
+  func open() {
+    print("Displaying files in \(name)...")
+    for file in files {
+      print("- \(file.name)")
+    }
+  }
+}
+
+let psychoKiller = Music(name: "Psycho Killer", artist: "The Talking Heads")
+let rebelRebel = Music(name: "Rebel Rebel", artist: "David Bowie")
+let blisterInTheSun = Music(name: "Blister in the Sun", artist: "Violent Femmes")
+let justKids = eBook(name: "Just Kids", author: "Patti Smith")
+
+var documents = Folder(name: "Documents")
+var musicFolder = Folder(name: "Great 70s Music")
+
+documents.addFile(musicFolder)
+documents.addFile(justKids)
+
+musicFolder.addFile(psychoKiller)
+musicFolder.addFile(rebelRebel)
+
+blisterInTheSun.open()
+justKids.open()
+print("")
+
+documents.open()
+print("")
+
+musicFolder.open()
+print("")
